3e30f769df47511ed1819b80aa9837d2f37fadf2
Nobutora Iyoda <nobysemail@gmail.com>
2025-01-08 00:48:54 -0500

    Populate HomePage with components. Enable buttons with text field is filled

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
+
 function HomePage() {
+  const [filled, setFilled] = useState(false)
+
   return (
-    <div>HOMEPAGE</div>
+    <Box>
+      <div>IMAGE</div>
+      <Typography variant="h3" gutterBottom>
+        Spyfall
+      </Typography>
+      <Box>
+        <div>Enter Your Name</div>
+        <TextField
+          placeholder="Player Name"
+          autoComplete="off"
+          onChange={(text) => setFilled(Boolean(text.target.value))}
+        />
+      </Box>
+      <Stack spacing={1}>
+        <Button variant="contained" disabled={!filled}>Create Lobby</Button>
+        <Button variant="contained"disabled={!filled}>Join Lobby</Button>
+      </Stack>
+    </Box>
   )
 }
 
