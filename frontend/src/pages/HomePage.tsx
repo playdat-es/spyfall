3fd3ad2aafbbbfc33c57a3cbad4ddc0bedd00b78
Noby1Kenobi <nobysemail@gmail.com>
2025-01-09 16:25:20 -0500

    Feature/homepage (#2)
    * Install Materials UI
    
    * Add empty homepage
    
    * Populate HomePage with components. Enable buttons with text field is filled
    
    * Center all elements in root div through the index.css
    
    * Remove App component
    
    * Add margin to seperate name input and lobby buttons
    
    * Add modal to allow users to input lobby code
    
    * Add placeholder image
    
    * Center text of modal and reduce size of headers
    
    * Remove debug function
    
    * Update names of state variables and set functions
    
    * Have hasName state variable hold the inputed text in the player name text field while continuing disable the buttons when empty
    
    * Set topography above name and lobby code text fields to labels

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material"
+import spyfallLogo from '../assets/react.svg'
+
+const lobbyCodeModalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 300,
+  bgcolor: 'background.paper',
+  color: 'black',
+  borderRadius: '10px',
+  p: 4,
+  textAlign: 'center',
+};
+
+function HomePage() {
+  const [name, setName] = useState('')
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <Box>
+      <img style={{height: '8em'}} src={spyfallLogo} className="logo" alt="Spyfall logo" />
+      <Typography variant="h4" sx={{ my: 4 }}>
+        Spyfall
+      </Typography>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="body1" component="label" gutterBottom>
+          Enter Your Name
+        </Typography>
+        <TextField
+          placeholder="Player Name"
+          autoComplete="off"
+          fullWidth
+          onChange={(text) => setName(text.target.value)}
+        />
+      </Box>
+      <Stack spacing={1}>
+        <Button variant="contained" disabled={!name}>Create Lobby</Button>
+        <Button variant="contained" disabled={!name} onClick={() => setShowModal(true)}>Join Lobby</Button>
+      </Stack>
+      <Modal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <Box sx={lobbyCodeModalStyle}>
+          <Typography variant="h4" component="label" gutterBottom>
+            Enter Lobby Code
+          </Typography>
+          <TextField
+            placeholder="Lobby Code"
+            autoComplete="off"
+            fullWidth
+          />
+        </Box>
+      </Modal>
+    </Box>
+  )
+}
+
+export default HomePage
