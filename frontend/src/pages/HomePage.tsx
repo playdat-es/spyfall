be3c2b4812b3977aa76190f17d703d315deb83d0
Nobutora Iyoda <nobysemail@gmail.com>
2025-01-09 22:12:58 -0500

    Create function to generate lobby code and navigate to lobby page with that code

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -21,6 +21,13 @@ function HomePage() {
   const [name, setName] = useState('')
   const [showModal, setShowModal] = useState(false)
 
+  // Call server to create lobby and navigate to lobby page with lobby code
+  // DEBUG CODE: lobby code is a random six digit number
+  const handleCreateLobby = () => {
+    const lobbyCode = Math.floor(100000 + Math.random() * 900000)
+    navigate(`/${lobbyCode}`)
+  }
+
   return (
     <Box>
       <img style={{height: '8em'}} src={spyfallLogo} className="logo" alt="Spyfall logo" />
@@ -39,7 +46,7 @@ function HomePage() {
         />
       </Box>
       <Stack spacing={1}>
-        <Button variant="contained" disabled={!name} onClick={() => navigate("/lobby")}>Create Lobby</Button>
+        <Button variant="contained" disabled={!name} onClick={handleCreateLobby}>Create Lobby</Button>
         <Button variant="contained" disabled={!name} onClick={() => setShowModal(true)}>Join Lobby</Button>
       </Stack>
       <Modal
