161d495dd7805dda622956549933f36badd5a744
James Chen <jameschen625@gmail.com>
2025-01-09 21:49:52 -0500

    address comments

--- backend/routes.py
+++ backend/routes.py
@@ -9,14 +9,11 @@ import string
 router = APIRouter()
 
 @router.post("/lobby", tags=["lobby"], response_description="Create a new lobby", status_code=status.HTTP_201_CREATED, response_model=CreateLobbyResponse)
-def create_book(request: Request, lobby: Lobby = Body(...)):
+def create_lobby(request: Request, lobby: Lobby = Body(...)):
     json_lobby = jsonable_encoder(lobby)
     code = random.choice(string.ascii_uppercase) + str(random.randint(0, 9)) + random.choice(string.ascii_uppercase) + str(random.randint(0, 9))
     json_lobby["code"] = code
 
     new_lobby = request.app.database["Lobby"].insert_one(json_lobby)
-    created_lobby = request.app.database["Lobby"].find_one(
-        {"_id": new_lobby.inserted_id}
-    )
 
-    return { "id": str(created_lobby["_id"]), "code": created_lobby["code"] }
+    return { "id": str(new_lobby.inserted_id), "code": code }
