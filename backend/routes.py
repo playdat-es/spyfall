97435614c8c127445c5986c8947f656d2000adc9
James Chen <jameschen625@gmail.com>
2025-01-09 21:18:15 -0500

    generate code on lobby creation

--- backend/routes.py
+++ backend/routes.py
@@ -3,11 +3,17 @@ from fastapi.encoders import jsonable_encoder
 from typing import List
 from models import Lobby, CreateLobbyResponse
 
+import random
+import string
+
 router = APIRouter()
 
 @router.post("/lobby", tags=["lobby"], response_description="Create a new lobby", status_code=status.HTTP_201_CREATED, response_model=CreateLobbyResponse)
 def create_book(request: Request, lobby: Lobby = Body(...)):
     lobby = jsonable_encoder(lobby)
+    code = random.choice(string.ascii_uppercase) + str(random.randint(0, 9)) + random.choice(string.ascii_uppercase) + str(random.randint(0, 9))
+    lobby["code"] = code
+
     new_lobby = request.app.database["Lobby"].insert_one(lobby)
     created_lobby = request.app.database["Lobby"].find_one(
         {"_id": new_lobby.inserted_id}
