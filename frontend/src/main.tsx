af83376802ab0f77095b41a9ed53b513366b3d9b
Nobutora Iyoda <nobysemail@gmail.com>
2025-01-09 20:57:45 -0500

    Manage routes for homepage and lobbypage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './pages/HomePage.tsx'
 import LobbyPage from './pages/LobbyPage.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePage />
-    <LobbyPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/lobby" element={<LobbyPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
