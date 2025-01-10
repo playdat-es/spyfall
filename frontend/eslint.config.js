bd8421312df0a6a430fd5d9050396623babfa4b8
Nobutora Iyoda <nobysemail@gmail.com>
2025-01-10 00:50:45 -0500

    Install prettier and add rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier";
+// import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,5 +12,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ];
